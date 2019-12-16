28782f5a7c970a0b5952fb0e19cd59ea2c42013e
kavitasid <kkavitasingh95@gmail.com>
2019-12-16 20:55:59 +0800

    Practice app by KavitaSid

---
+++ app/src/main/java/com/kavisid/practical/example/activity/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.kavisid.practical.example.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kavisid.practical.example.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
